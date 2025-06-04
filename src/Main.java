2592da13ede056a989e4d115a0b4c3a6cb6ea88d
JuanGallardo373 <juanigallardo152@gmail.com>
2025-06-04 19:09:35 -0300

    Poda, boton resolver y cambios en encontrarcamino y main

--- src/Main.java
+++ src/Main.java
@@ -9,36 +9,8 @@ public class Main {
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    int filas = 9; // Número de filas
-                    int columnas = 10; // Número de columnas
-                    MainWindow window = new MainWindow();
-                    window.frame.setVisible(true);
                     Controlador controlador = new Controlador();
-                    controlador.crearTablero(filas, columnas);
-
-                    // Ejecutar crearTabla en un hilo separado para evitar bloquear la interfaz de
-                    // usuario
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            window.crearTabla(filas, columnas, controlador.getTablero().getCeldas());
-                            window.repintar();
-                        }
-                    }).start();
-
-                    // Ejecutar encontrarCaminoValido en un hilo separado
-                    new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            try {
-                                Thread.sleep(1000); // Esperar un segundo antes de buscar el camino
-                                window.pintarTablero(controlador.encontrarCaminoValido());
-                            } catch (InterruptedException e) {
-                                e.printStackTrace();
-                            }
-                        }
-                    }).start();
-
+                    controlador.abrirVista();
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
--- src/vista/MainWindow.java
+++ src/vista/MainWindow.java
@@ -1,17 +1,30 @@
 package vista;
 
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+
+import controller.Controlador;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
 
 public class MainWindow {
 
 	public JFrame frame;
+	private Controlador controlador;
 	private JTextField[][] textField;
+	private JPanel panelTablero;
 
-	public MainWindow() {
+	public MainWindow(Controlador controlador) {
+		this.controlador = controlador;
 		initialize();
 	}
 
@@ -19,47 +32,79 @@ public class MainWindow {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+	    frame = new JFrame();
+	    frame.setSize(1000, 800);
+		frame.setLayout(new BorderLayout());
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        JButton btnCargarTablero = new JButton("Seleccionar tablero");
+        JButton btnResolver = new JButton("Resolver");
+
+        JPanel topPanel = new JPanel();
+        topPanel.add(btnCargarTablero);
+        topPanel.add(btnResolver);
+
+//        btnCargarTablero.addActionListener(this::seleccionarTablero);
+        btnResolver.addActionListener(this::encontrarCaminoValido);
+        
+        frame.add(topPanel, BorderLayout.NORTH);
+        
+        JButton btnResultados = new JButton("Resultados");
+//        btnResultados.addActionListener(this::obtenerResultados);
+        
+        frame.add(btnResultados, BorderLayout.SOUTH);
+        
+
+		controlador.crearTablero(9, 10);
+		controlador.dibujarTabla();
 	}
 
-	public void crearTabla(int filas, int columnas, Boolean[][] celdas) {
+	public void dibujarTabla(int filas, int columnas, Boolean[][] celdas) {
 		textField = new JTextField[filas][columnas];
-		frame.setLayout(new GridLayout(filas, columnas));
+	    // Si ya había una tabla previa, la removemos
+	    if (panelTablero != null) frame.remove(panelTablero);
+	    
+	    panelTablero = new JPanel(new GridLayout(filas, columnas, 2, 2));
+	    
 		for (int i = 0; i < filas; i++) {
 			for (int j = 0; j < columnas; j++) {
-				if (celdas[i][j]) {
-					textField[i][j] = new JTextField(); // Inicializar el botón
-					textField[i][j].setText(" 1");
-					frame.add(textField[i][j]); // Agregar el botón al frame
-				} else {
-					textField[i][j] = new JTextField(); // Inicializar el botón
-					textField[i][j].setText("-1");
-					frame.add(textField[i][j]); // Agregar el botón al frame
-				}
+				textField[i][j] = new JTextField(); // Inicializar el botón
+				textField[i][j].setHorizontalAlignment(JTextField.CENTER);
+	            textField[i][j].setEditable(false);
+	            textField[i][j].setText(celdas[i][j] ? "1" : "-1");
+				panelTablero.add(textField[i][j]); // Agregar el botón al frame
 			}
 		}
+		frame.add(new JScrollPane(panelTablero), BorderLayout.CENTER);
+		repintar();
 	}
 
+	private void encontrarCaminoValido(ActionEvent e) {
+		controlador.encontrarCaminoValido(true);
+	}
 	public void pintarTablero(ArrayList<Boolean> movimientos) {
 		int x = 0, y = 0;
-		for (int i = 0; i < movimientos.size(); i++) {
-			if (movimientos.get(i)) {
-				textField[x][y].setBackground(Color.GREEN); // Pintar de verde si se mueve a la derecha
+		textField[x][y].setBackground(Color.GREEN); // Pintar de verde primer casillero
+		for (Boolean movimiento : movimientos) {
+			if (movimiento)
 				y++; // Movimiento a la derecha
-
-			} else {
-				textField[x][y].setBackground(Color.GREEN); // Pintar de verde si se mueve abajo
+			else 
 				x++; // Movimiento hacia abajo
-			}
+			textField[x][y].setBackground(Color.GREEN); // Pintar de verde
 		}
-		textField[x][y].setBackground(Color.GREEN); // Pintar de verde ultimo casillero
 		repintar();
 	}
 
 	public void repintar() {
 		frame.revalidate();
 		frame.repaint();
+	}
+
+	public static void lanzarError(String mensaje) {
+		JOptionPane.showMessageDialog(
+				null,
+				mensaje,
+				"Error",
+				JOptionPane.ERROR_MESSAGE);
 	}
 }
